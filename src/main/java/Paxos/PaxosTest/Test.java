26e8b4b319a8c3beaf8e868a42a3037597cd37b8
Marco Prosdocimi <prosdino95@gmail.cpm>
2019-08-21 11:19:57 +0200

    last fix for paxos timer

--- src/main/java/Paxos/PaxosTest/Test.java
+++ src/main/java/Paxos/PaxosTest/Test.java
@@ -12,13 +12,13 @@ public class Test {
         Scanner s = new Scanner(System.in);
         Random rng = new Random();
 
-        AgentHandler a1 = new AgentHandler((long)1,"./src/main/java/Paxos/PaxosTest/");
+        AgentHandler a1 = new AgentHandler((long)rng.nextLong(),"./src/main/java/Paxos/PaxosTest/");
         Thread t = new Thread(a1);
         t.start();
-        AgentHandler a2 = new AgentHandler((long)2,"./src/main/java/Paxos/PaxosTest/");
+        AgentHandler a2 = new AgentHandler((long)rng.nextLong(),"./src/main/java/Paxos/PaxosTest/");
         Thread t2 = new Thread(a2);
         t2.start();
-        AgentHandler a3 = new AgentHandler((long)3,"./src/main/java/Paxos/PaxosTest/");
+        AgentHandler a3 = new AgentHandler((long)rng.nextLong(),"./src/main/java/Paxos/PaxosTest/");
         Thread t3 = new Thread(a3);
         t3.start();
 
@@ -31,7 +31,7 @@ public class Test {
       //  System.out.println(a3.getid()+" think java is wonderful");
 
 
-        a3.propose("java i1s wonderful",a3.getid());
+        a3.propose("java is wonderful",250);
 
         //Thread.sleep(1000);
         a2.propose("java sucks",12);
